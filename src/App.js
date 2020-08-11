845a6d3019d212e18266cd4eb97782dda8932dad
abazurek <skala2294@gmail.com>
2020-08-11 17:12:47 +0200

    structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'reset-css';
+import './scss/main.scss';
+
+import Main from "./components/Main";
+
+
+function App() {
+  return (
+    <div className="App">
+        <div className='margin'/>
+        <Main napis='jakieś głupoty'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Main({napis}) {
+
+    return(
+        <div> {napis}</div>
+    )
+}
+
+export default Main
