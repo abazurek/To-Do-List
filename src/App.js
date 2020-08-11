773f11c785aabb604ca029f923d7747fd9ebd9d6
abazurek <skala2294@gmail.com>
2020-08-11 17:14:11 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
         <div className='margin'/>
-        <Main napis='jakieś głupoty'/>
+        <Main napis='jakieś rzeczy'/>
     </div>
   );
 }
