60f121a6d88568249f3585121d569322b38d77da
abazurek <skala2294@gmail.com>
2020-08-12 20:02:29 +0200

    done good hide Input task

--- src/components/TaskBox.js
+++ src/components/TaskBox.js
@@ -22,7 +22,7 @@ function TaskBox({title, count, toDo, progress, done}) {
                 <h3 className='task-title'>{title} <span>({count})</span></h3>
                 <button className='add-button' onClick={clickButton}><span>Dodaj</span> <FontAwesomeIcon icon={faPlus}/></button>
             </div>
-            <div>{newTask? <SingleTask mainTitle={title} title='Wpisz tytuł..' message="Tutaj wpisz opis.."  input={true}/> : ''}
+            <div>{newTask? <SingleTask mainTitle={title} title='Wpisz tytuł..' message="Tutaj wpisz opis.."  input={true} showInput={setNewTask}/> : ''}
                 {toDo && title==='Do zrobienia'? toDo.map(item=> <SingleTask key={item.id} title={item.title} message={item.message} item={item} symbole='circle' input={false} buttons={["W trakcie", "Zrobione"]}/>)  :'' }
                 {progress && title==='W trakcie'? progress.map(item=> <SingleTask key={item.id} title={item.title} message={item.message} item={item} symbole='edit' input={false} buttons={["Do zrobienia", "Zrobione"]}/>)  :''}
                 {done && title==='Zrobione'? done.map(item=> <SingleTask key={item.id} title={item.title} message={item.message} item={item} symbole='done' input={false} buttons={["Do zrobienia", "W trakcie"]}/>)  :''}
